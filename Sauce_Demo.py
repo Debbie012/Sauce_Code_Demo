cb58119f6ad0c52a8e198c61b7204e7c4a20f6f7
Debbie Ntigeli <ntigelidebbie@gmail.com>
2025-03-14 00:00:54 +0100

    update

--- Sauce_Demo.py
+++ Sauce_Demo.py
@@ -3,53 +3,48 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# Define Variables
+URL = "https://www.saucedemo.com/inventory.html"
+USERNAME = "standard_user"
+PASSWORD = "secret_sauce"
+WAIT_TIME = 5
+
+# List of products to add to cart
+PRODUCT = [
+    "add-to-cart-sauce-labs-backpack",
+    "add-to-cart-sauce-labs-bike-light",
+    "add-to-cart-sauce-labs-bolt-t-shirt",
+    "add-to-cart-sauce-labs-fleece-jacket",
+    "add-to-cart-sauce-labs-onesie",
+    "add-to-cart-test.allthethings()-t-shirt-(red)"
+]
+
 # Initialize Chrome browser instance using Selenium WebDriver
 driver = webdriver.Chrome()
 
 # Navigate to the login page of Sauce Demo
-driver.get("https://www.saucedemo.com/inventory.html")
+driver.get(URL)
 time.sleep(5)
 
-# Login with a valid username
-driver.find_element(By.ID,"user-name").send_keys("standard_user")
-time.sleep(5)
+# Perform Login
+driver.find_element(By.ID,"user-name").send_keys(USERNAME)
+time.sleep(WAIT_TIME)
 
-# Enter Password
-driver.find_element(By.ID,"password").send_keys("secret_sauce")
-time.sleep(5)
+driver.find_element(By.ID,"password").send_keys(PASSWORD)
+time.sleep(WAIT_TIME)
 
-# Click Login
 driver.find_element(By.ID,"login-button").click()
-time.sleep(5)
+time.sleep(WAIT_TIME)
 
-# Add a backpack to cart
-driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack").click()
-time.sleep(5)
-
-# Add bike light to cart
-driver.find_element(By.ID,"add-to-cart-sauce-labs-bike-light").click()
-time.sleep(5)
-
-# Add T-Shirt to cart
-driver.find_element(By.ID,"add-to-cart-sauce-labs-bolt-t-shirt").click()
-time.sleep(5)
-
-# Add Jacket to cart
-driver.find_element(By.ID,"add-to-cart-sauce-labs-fleece-jacket").click()
-time.sleep(5)
-
-# Add Onesie to cart
-driver.find_element(By.ID,"add-to-cart-sauce-labs-onesie").click()
-time.sleep(5)
-
-# Add Red T-Shirt to cart
-driver.find_element(By.ID,"add-to-cart-test.allthethings()-t-shirt-(red)").click()
-time.sleep(5)
+# Add product to cart using a loop
+for product_id in PRODUCT:
+    driver.find_element(By.ID, product_id).click()
+time.sleep(WAIT_TIME)
 
 # Click on the burger menu
 driver.find_element(By.ID,"react-burger-menu-btn").click()
-time.sleep(5)
+time.sleep(WAIT_TIME)
 
 # Click on Logout
 driver.find_element(By.ID,"logout_sidebar_link").click()
-time.sleep(5)
+time.sleep(WAIT_TIME)
