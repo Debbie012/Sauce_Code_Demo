657cd5c19ceb94ad124f93fb216664d28000a42b
Debbie Ntigeli <ntigelidebbie@gmail.com>
2025-03-14 13:25:36 +0100

    update

--- Sauce_Demo.py
+++ Sauce_Demo.py
@@ -4,10 +4,36 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # Define Variables
-URL = "https://www.saucedemo.com/inventory.html"
+URL = "https://www.saucedemo.com/"
 USERNAME = "standard_user"
 PASSWORD = "secret_sauce"
 WAIT_TIME = 5
+
+# Initialize Chrome browser instance using Selenium WebDriver
+driver = webdriver.Chrome()
+
+# Navigate to the login page of Sauce Demo
+driver.get(URL)
+time.sleep(WAIT_TIME) # Wait for page to load
+
+# Define Elements as Variables
+Username = driver.find_element(By.ID,"user-name")
+Password = driver.find_element(By.ID,"password")
+Login = driver.find_element(By.ID,"login-button")
+
+# Perform Login
+Username.send_keys(USERNAME)
+time.sleep(WAIT_TIME)
+
+Password.send_keys(PASSWORD)
+time.sleep(WAIT_TIME)
+
+Login.click()
+time.sleep(WAIT_TIME)
+
+# Define Menu and Logout buttons
+menu = driver.find_element(By.ID,"react-burger-menu-btn")
+Logout = driver.find_element(By.ID,"logout_sidebar_link")
 
 # List of products to add to cart
 PRODUCT = [
@@ -19,32 +45,18 @@ PRODUCT = [
     "add-to-cart-test.allthethings()-t-shirt-(red)"
 ]
 
-# Initialize Chrome browser instance using Selenium WebDriver
-driver = webdriver.Chrome()
-
-# Navigate to the login page of Sauce Demo
-driver.get(URL)
-time.sleep(5)
-
-# Perform Login
-driver.find_element(By.ID,"user-name").send_keys(USERNAME)
-time.sleep(WAIT_TIME)
-
-driver.find_element(By.ID,"password").send_keys(PASSWORD)
-time.sleep(WAIT_TIME)
-
-driver.find_element(By.ID,"login-button").click()
-time.sleep(WAIT_TIME)
-
 # Add product to cart using a loop
 for product_id in PRODUCT:
     driver.find_element(By.ID, product_id).click()
 time.sleep(WAIT_TIME)
 
 # Click on the burger menu
-driver.find_element(By.ID,"react-burger-menu-btn").click()
+menu.click()
 time.sleep(WAIT_TIME)
 
 # Click on Logout
-driver.find_element(By.ID,"logout_sidebar_link").click()
+Logout.click()
 time.sleep(WAIT_TIME)
+
+# Quit browser
+driver.quit()
